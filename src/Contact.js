95d6cb3ce7f8f563a024f1b0f5cf6bea56dbe91d
stephlane544 <stephlane544@gmail.com>
2019-06-17 22:03:33 -0600

    functional contact form

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-const API_PATH = 'http://localhost:3000/portfolio/api/contact/index.php';
+var email 	= require('emailjs');
 
 
 
@@ -8,7 +7,7 @@ class Contact extends Component {
     constructor() {
         super();
         this.state = {
-            firstName: '',
+            name: '',
             email: '',
             message:'',
             mailSent: false,
@@ -22,51 +21,35 @@ class Contact extends Component {
 
     handleSubmit = e => {
         e.preventDefault();
-        axios({
-            method: 'post',
-            url: `${API_PATH}`,
-            headers: { 'content-type': 'application/json' },
-            data: this.state
-        })
-        .then(result => {
-            this.setState({
-            mailSent: result.data.sent
-            })
-        })
-        .catch(error => this.setState({ error: error.message }));
+      
     }
 
     render() {
         return (
-            <form className='contactPage form' action="/action_page.php" >
-            <input type="text"
-                placeholder='First Name'
-                name='firstName'
-                value={this.state.firstName}
-                onChange={this.handleChange}
-                required
-            />
-            <input type="text"
-                placeholder='Email'
-                name='email'
-                value={this.state.email}
-                onChange={this.handleChange}
-                required
-            />  
-            <textarea type="text"
-                placeholder='Message'
-                name='message'
-                value={this.state.message}
-                onChange={this.handleChange}
-                required
-            />           
-            <button className='submitButton'>Send Message</button>
-            <div>
-                {this.state.mailSent &&
-                    <div>Thank you for contcting us.</div>
-                }
-            </div>
-        </form>
+            <form className='contactPage form' action="contact-form-handler.php"  method="post">
+                <input type="text"
+                    placeholder='First Name'
+                    name='name'
+                    // value={this.state.name}
+                    // onChange={this.handleChange}
+                    required
+                />
+                <input type="email"
+                    placeholder='Email'
+                    name='email'
+                    // value={this.state.email}
+                    // onChange={this.handleChange}
+                    required
+                />  
+                <textarea type="text"
+                    placeholder='Message'
+                    name='message'
+                    // value={this.state.message}
+                    // onChange={this.handleChange}
+                    required
+                />           
+                <button className='submitButton'>Send Message</button>
+            </form>
         );
     }
 }

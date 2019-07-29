dce86801f86aa4464722223f5a62e0e7ec9798cd
stephlane544 <stephlane544@gmail.com>
2019-07-28 20:47:59 -0600

    updated resume

--- src/Resume.js
+++ src/Resume.js
@@ -5,7 +5,8 @@ import './Styles/Resume.css';
 const Resume = () => {
     return (
         <div className="resumeContainer">
-            <iframe title='resume' src="https://www.keepandshare.com/doc16/23488/lane-stephanie-resume-pdf-348k?da=y&ifr=y" scrolling="yes" ></iframe>
+            <iframe title='resume' src="https://www.keepandshare.com/doc16/23499/2816265?da=y&ifr=y" scrolling="yes" ></iframe>
+            
         </div>
     );
 };
